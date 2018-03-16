c9196aa245bc6e7bc31cd7fe2e92579914a8b072
annetawamono <annetawamono@yahoo.com.au>
2018-03-16 10:26:39 +0200

    Deleted logs

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function() {
   $('#intro div.nav-buttons>i').click(navIntro);
 
   //  Selection Event Handlers
-  var movementStrength = 5;
+  var movementStrength = 10;
   $selection = $("#selection div#content");
   var height;
   var width;
@@ -13,11 +13,8 @@ $(function() {
     width = movementStrength / $selection.width();
     var pageX = (e.pageX - ($selection.width() / 2))
     var pageY = (e.pageY - ($selection.height() / 2))
-    console.log("height:"+height+" width:"+width);
-    console.log("pageX:"+pageX+" pageY:"+pageY);
     var newvalueX = -1*((width * (pageX * 1)) - 50);
     var newvalueY = -1*((height * (pageY * -1)) - 105);
-    console.log("newvalueX"+newvalueX+" newvalueY"+newvalueY);
     $('#selection div#content').css("background-position", newvalueX + "%     " + newvalueY + "%");
   });
 });
