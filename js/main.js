b01af4e0861289da4036d6c9510766e7cc3bade6
annetawamono <annetawamono@yahoo.com.au>
2018-03-16 22:09:52 +0200

    Start of animations

--- js/main.js
+++ js/main.js
@@ -11,10 +11,25 @@ $(function() {
   $selection.mousemove(function(e) {
     height = movementStrength / $selection.height();
     width = movementStrength / $selection.width();
-    var pageX = (e.pageX - ($selection.width() / 2))
-    var pageY = (e.pageY - ($selection.height() / 2))
+    var pageX = (e.pageX - ($selection.width() / 2));
+    var pageY = (e.pageY - ($selection.height() / 2));
     var newvalueX = -1*((width * (pageX * 1)) - 50);
     var newvalueY = -1*((height * (pageY * -1)) - 105);
     $('#selection div#content').css("background-position", newvalueX + "%     " + newvalueY + "%");
   });
+
+  //  Selection Title Anim
+  const element = document.querySelector('#content h1');
+  charming(element);
+
+  var titleAnim = anime({
+    targets: '#content h1#title-1>span',
+    rotateZ: [-180,180],
+    // opacity: 1,
+    easing: 'easeInCubic',
+    delay: function(el, i, l) { return i * 100; },
+    autoplay: false
+  });
+
+  $('#content h1').click(titleAnim.play);
 });
